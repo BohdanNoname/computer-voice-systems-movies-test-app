9a471cb221d5d6a55400fa09d09a5e3544565435
BohdanNoname <nedashkovskiy1319@gmail.com>
2023-12-28 10:19:59 +0200

    rewriting onclick function for movie item

--- app/src/main/java/ua/nedash/movies/presentation/catalog/adapter/MoviesAdapter.kt
+++ app/src/main/java/ua/nedash/movies/presentation/catalog/adapter/MoviesAdapter.kt
@@ -13,12 +13,22 @@ import ua.nedash.movies.extensions.setImageUrlWithGlide
 
 class MoviesAdapter(
     private val onMovieClick: (Movie) -> Unit
-) : ListAdapter<Movie, MoviesAdapter.MoviesViewHolder>(MovieDiffCallback) {
+) : ListAdapter<Movie, MoviesAdapter.MoviesViewHolder>(MovieDiffCallback), View.OnClickListener {
+
+    override fun onClick(v: View?) {
+        val movie = v?.tag as? Movie ?: return
+
+        when (v.id) {
+            R.id.cl_item_movie -> onMovieClick(movie)
+        }
+    }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MoviesViewHolder {
         val binding = ItemMovieBinding.inflate(
             LayoutInflater.from(parent.context), parent, false
         )
+
+        binding.root.setOnClickListener(this)
 
         return MoviesViewHolder(binding)
     }
@@ -29,16 +39,17 @@ class MoviesAdapter(
     inner class MoviesViewHolder(private val binding: ItemMovieBinding) :
         RecyclerView.ViewHolder(binding.root) {
 
-        init {
-            binding.root.setOnClickListener {
-                onMovieClick(currentList[bindingAdapterPosition])
-            }
-        }
+//        init {
+//            binding.root.setOnClickListener {
+//                onMovieClick(currentList[bindingAdapterPosition])
+//            }
+//        }
 
         private val context = binding.root.context
 
         fun bind(movie: Movie) {
             with(binding) {
+                clItemMovie.tag = movie
                 tvIsOnWatchlist.visibility = if (movie.isOnWatchlist) View.VISIBLE else View.GONE
 
                 ivCoverArt.setImageUrlWithGlide(movie.coverArtUrl)
